02c7ee80fba95048d3ce0bcf98049a189f6d8808
ImBary <bartek.sm@outlook.com>
2024-04-04 21:53:00 +0200

    monte na chama

--- Park.java
+++ Park.java
@@ -181,5 +181,58 @@ public class Park {
         }
         return maxError;
     }
+
+
+
+    //MONTE
+    public double monte(int N, int wanderer, ArrayList<Integer> exits, ArrayList<Integer> osk){
+        double prob=0.0;
+        for(int i = 0; i < N; i++) {
+            if (singleTest(wanderer, exits, osk)) {
+                prob++;
+            }
+        }
+        return prob/N;
+    }
+
+
+    public Boolean singleTest(int start, ArrayList<Integer> exits, ArrayList<Integer> osk) {
+        Random rand = new Random();
+        Position wanderer = new Position(start, 0);
+        while (!osk.contains(wanderer.getIntersection()) && !exits.contains(wanderer.getIntersection())) {
+            if (wanderer.alley == null) {
+                List<Alley> possibleAlleys = new ArrayList<>();
+                for (Alley alley : alleys) {
+                    if (alley.getA().getId() == wanderer.getIntersection() || alley.getB().getId() == wanderer.getIntersection()) {
+                        possibleAlleys.add(alley);
+                    }
+                }
+                wanderer.alley = possibleAlleys.get(rand.nextInt(possibleAlleys.size()));
+                wanderer.distance++;
+            } else {
+                int step = rand.nextInt(2);
+                if (step == 0) {
+                    wanderer.distance--;
+                } else {
+                    wanderer.distance++;
+                }
+                if (wanderer.distance == 0) {
+                    wanderer.alley = null;
+                } else if ( wanderer.distance == wanderer.alley.getLength()) {
+                    if (wanderer.alley.getA().getId() != wanderer.intersection) {
+                        wanderer.intersection = wanderer.alley.getA().getId();
+                    } else {
+                        wanderer.intersection = wanderer.alley.getB().getId();
+                    }
+                }
+            }
+        }
+        if (osk.contains(wanderer.getIntersection())){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
 
